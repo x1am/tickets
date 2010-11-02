e83d39f9c7eb45bebe1f9a74d6572eb316fe095d
x1am <razy.backend@gmail.com>
2010-11-02 18:05:50 +0300

    utf8 bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def reltime(time)
     #d = time < 1.day.ago ? "позавчера" : "вчера"
     #str = time_ago_in_words(time) == "1 день" ? "#{d} в #{time.to_s :time}" : "#{time_ago_in_words(time)} назад"
-    str = "#{time_ago_in_words(time)} назад".force_encoding("UTF-8")
+    str = "#{time_ago_in_words(time).force_encoding("UTF-8")} назад"
     #str += time < 1.day.ago ? " в #{time.to_s :time}" : ''
     "<abbr title='#{Russian::strftime(time,'%A %e %B %H:%M')}'>#{str}</abbr>"
   end
